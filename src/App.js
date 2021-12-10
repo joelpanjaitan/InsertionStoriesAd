e6e50b31f056ece5ef428915e9890559375eafa1
joelpanjaitan <metrohal88@gmail.com>
2021-12-10 14:17:59 +0700

    application interface fix

--- src/App.js
+++ src/App.js
@@ -6,26 +6,44 @@ import { Stack, Grid } from "@mui/material";
 function App() {
   return (
     <div className="App">
-      {/* <header className="App-header"> */}
-      <h1>Hello World</h1>
-      <Button varian="contained">Hello World</Button>
-      {/* </header> */}
-      <Grid item sm={12} xs={12} sx={{ mb: 2, mt: 1 }}>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          spacing={2}
-        >
-          <h2>Home</h2>
-          <h2>Liveevent</h2>
-          <h2>Livetv</h2>
-        </Stack>
-      </Grid>
-      <Routes>
-        <Route path="/progress" element={<Progress />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+      <div className="Grey-bg">
+        <h1>Original movies</h1>
+        <Button varian="contained">Stories here</Button>
+
+        <Grid item sm={12} style={{ display: "flex" }} xs={12} sx={{ mb: 12 }}>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={4}
+          >
+            <div className="circle">
+              <p class="text">Circle here</p>
+            </div>
+            <div className="circle">
+              <p class="text">Circle here</p>
+            </div>
+          </Stack>
+        </Grid>
+        <Grid item sm={12} xs={12} sx={{ mb: 2, mt: 1 }}>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            spacing={2}
+          >
+            <p>Home</p>
+            <p>Liveevent</p>
+            <p>Livetv</p>
+            <p>Library</p>
+            <p>Account</p>
+          </Stack>
+        </Grid>
+        <Routes>
+          <Route path="/progress" element={<Progress />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 }
